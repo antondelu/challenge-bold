63a89d034d731a8ed202cfdc3346f8dc3290f79b
antondl <marcoanton.deluca@gmail.com>
2024-02-09 10:17:27 -0300

    Initial commit challenge BOLD

---
+++ src/components/dataTable/constants.ts
@@ -0,0 +1,32 @@
+export const rows = [
+  {
+    id: 1,
+    complete: true,
+    name: "Customizar theme",
+    description: "Customizar theme de MUI",
+  },
+  {
+    id: 2,
+    complete: true,
+    name: "Buildear app",
+    description: "",
+  },
+  {
+    id: 3,
+    complete: false,
+    name: "Instalar pnpm",
+    description: "En esta tarea se busca reemplazar NPM por PNPM ",
+  },
+  {
+    id: 4,
+    complete: false,
+    name: "Redux ",
+    description: "",
+  },
+  {
+    id: 5,
+    complete: true,
+    name: "Dynamo",
+    description: "",
+  },
+];
